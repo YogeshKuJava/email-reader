79d2f2786722bba737a4820947f06b9f29a0dace
YogeshMagellan <kumary@magellanhealth.com>
2024-02-02 17:23:02 +0530

    updated

--- src/main/java/email/reader/ApiService.java
+++ src/main/java/email/reader/ApiService.java
@@ -1,38 +1,38 @@
 package email.reader;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class ApiService {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ApiService.class);
 
-	private final WebClient webClient;
+	public void callExternalApiAsync(String url) {
+		AsyncHttpClient client = new DefaultAsyncHttpClient();
+		try {
+			client.prepare("POST", "https://www.virustotal.com/api/v3/urls").setHeader("accept", "application/json")
+					.setHeader("x-apikey", "86138a91c63d79dcaa45a30e1a912f8264603e9986282339706df2d9fe2fb21f")
+					.setHeader("content-type", "application/x-www-form-urlencoded").setBody("url=" + url).execute()
+					.toCompletableFuture().thenAccept(System.out::println).join();
 
-	public ApiService(WebClient.Builder webClientBuilder) {
-		this.webClient = webClientBuilder.baseUrl("https://www.virustotal.com").build();
-
+			client.close();
+		} catch (IOException e) {
+			logger.error("Exception in calling virustotal api " + e.getMessage() + " Cause : " + e.getCause());
+		}
 	}
 
-	public CompletableFuture<String> callExternalApiAsync(String url) {
-		System.out.println("Coming here .............................1111111111111111"+url);
-		return webClient.post().uri("/api/v3/urls").header("accept", "application/json")
-				.header("x-apikey", "86138a91c63d79dcaa45a30e1a912f8264603e9986282339706df2d9fe2fb21f")
-				.header("content-type", "application/x-www-form-urlencoded").bodyValue("url=" + url).retrieve()
-				.bodyToMono(String.class).toFuture();
-	}
-	
 	public void validateURLS(List<String> urlList) {
-		for(String url: urlList) {
-			System.out.println("Coming here .............................222222222222222222222222-size ->"+urlList.size());
+		for (String url : urlList) {
 
-			logger.info(url, callExternalApiAsync(url).thenAccept(result -> System.out.println("Result of url : "+url+ "is : -->" + result)));
+			logger.info("For url " + url + " Respose is ");
+			callExternalApiAsync(url);
 		}
 	}
 }
